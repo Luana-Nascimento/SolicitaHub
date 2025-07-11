58b93ce0f8063b6ef4a6fc9c29844c0c6bab0aef
Luana-Nascimento <luananascimento00@outlook.com>
2025-07-11 18:24:44 -0300

    Initial commit with Maven project structure

---
+++ src/main/java/com/solicitahub/SolicitaHubApplication.java
@@ -0,0 +1,13 @@
+package com.solicitahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolicitaHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolicitaHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solicitahub/SolicitaHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solicitahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolicitaHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
